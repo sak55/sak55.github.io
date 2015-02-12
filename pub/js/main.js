f05a80a5853f324615fbdeb288dfd0d3180a257e
Sam Kauffman <samuelkauffman@gmail.com>
2015-02-12 16:16:28 -0500

    added sliding contact box

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,1 +1,17 @@
 
+$(document).ready(function () {
+   
+    
+    $('#contact').click(function() {
+       if ( $( "#formBox" ).is( ":hidden" ) ) {
+            $( "#formBox" ).slideDown( "slow" );
+        } else {
+            $( "#formBox" ).hide();
+  }
+    });
+    
+        
+     
+    
+ 
+});
